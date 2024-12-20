f6c6aee1b4c699c3b65f0417245d890208b4d1b2
SandwichhDev <reecewiswell@gmail.com>
2024-12-20 02:58:51 -0600

    Added sfx to obstacle
    Added SFX to obstacle. Added assets and sfx folders. Starting importing some models to use for levels.

--- props/power_cell.gd
+++ props/power_cell.gd
@@ -2,6 +2,7 @@ class_name PowerCell extends MeshInstance3D
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	_collect_power_cell()
+	
 
 func _collect_power_cell() -> void:
 	print("Power cell collected")
