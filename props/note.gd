12a20855dfedb287cd132d4b32b341bcc3d2e24e
Grant Wiswell <wiswellgrant@gmail.com>
2024-12-20 20:23:35 -0600

    Added interactable objects

---
+++ props/note.gd
@@ -0,0 +1,40 @@
+@tool
+class_name Note extends Interactable
+
+@export var mesh: Mesh
+@export var background: Texture2D
+@export_multiline var text: String = ""
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	super()
+	$Control/Label.text = text
+	$Control.hide()
+	$MeshInstance3D.mesh = mesh
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func start_interact() -> void:
+	super()
+
+func do_interact():
+	if is_interacting:
+		end_interact()
+		return
+	super()
+	_hide_tooltip()
+	$Control.show()
+
+func end_interact() -> void:
+	super()
+	$Control.hide()
+
+func stop_interact() -> void:
+	super()
+	$Control.hide()
+
+func _validate_property(property: Dictionary) -> void:
+	if property.name == "mesh":
+		$MeshInstance3D.mesh = mesh
